09526bb186e5b1ec5d90d7caaaa40a2efc03c7bc
drg2789 <dnikam541@gmail.com>
2024-06-17 21:01:28 +0530

    Create README.md7

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,4 +1,5 @@
 a=20
 b=10
 c=89
+d=67
 print("sum {0} , {1} and {2} is {3}".format(a,b,c,a+b+c+g))
--- student_assignment.py
+++ student_assignment.py
@@ -1,9 +1,44 @@
 import os
-files=os.listdir(r'C:\Users\Admin\Desktop\student_data')
-print(files)
+import csv
 
-all_csv_files=[]
-for i in files:
-    if i.endswith(".csv"):
-        all_csv_files.append(i)
-print(f"following are {all_csv_files} csv files")
+Path=r'E:\New folder\Student_data'
+def csv_files(path):
+
+    try:
+        files = os.listdir(path)
+        all_csv_files = []
+        for  i in files:
+             j=Path +'\\' +i
+             if j.endswith(".csv"):
+                all_csv_files.append(j)
+        return all_csv_files
+    except Exception as e:
+           print(f'erorr:{e}')
+files=csv_files(Path)
+def read_csv_file(csv_file_path):
+        with open( csv_file_path,mode='r')as file1:
+              csvfile=csv.reader(file1)
+              header=next(csvfile)
+              header.extend(['total','per'])
+              data=[]
+              for  file in  csvfile:
+                   total=int(file[3])+int(file[4])+int(file[5])+int(file[6])+int(file[7])
+                   per=total / 5
+                   file.extend([total,per])
+                   #data.append(total)
+                   #data.append(per)
+                   data.append(file)
+                  #  print(file)
+        print(data)
+        return data,header
+
+
+all_data=[]
+for file_path in files:
+     data,header=read_csv_file(file_path)
+     all_data.extend(data)
+
+
+
+
+
